3f27f2fc38b0f5c2b15470ab2a08cfc0528e1791
ChiefAnthony <anthonystudie@gmail.com>
2024-11-06 02:25:48 +0100

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,22 +1,37 @@
-import socket  # noqa: F401
+import socket
+import select
 
 
 def main():
-    print("Logs from your program will appear here!")
-
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    client, addr = server_socket.accept()
-    # client.send(b"+PONG\r\n")
+    server_socket.setblocking(False)  # Make it non-blocking
+
+    sockets_to_monitor = [server_socket]
+
+    print("Server is ready to accept connections...")
 
     while True:
-        data = client.recv(1024)
-        if not data:
-            break
+        ready_to_read, _, _ = select.select(sockets_to_monitor, [], [])
 
-        client.send(b"+PONG\r\n")
+        for sock in ready_to_read:
+            if sock == server_socket:
+                client_socket, address = server_socket.accept()
+                client_socket.setblocking(False)
+                sockets_to_monitor.append(client_socket)
+                print(f"New connection from {address}")
 
-    client.close()
-    server_socket.close()
+            else:
+                try:
+                    data = sock.recv(1024)
+                    if data:
+                        sock.send(b"+PONG\r\n")
+                    else:
+                        raise ConnectionResetError
+
+                except (ConnectionResetError, BrokenPipeError):
+                    print("Client disconnected")
+                    sockets_to_monitor.remove(sock)
+                    sock.close()
 
 
 if __name__ == "__main__":
